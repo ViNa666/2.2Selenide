1c960f01002c62a5b966ce9ac94ec09841343395
ViNa666 <legenda-2000@yandex.ru>
2022-04-10 12:30:38 +0300

    improved1

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,11 +2,14 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 
@@ -20,33 +23,29 @@ public class CardDeliveryTest {
     @Test
     public void shouldOrderCardDelivery () {
 
-        //Настройка актуальной валидной даты
-        Date today = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-        long todayTime = today.getTime();                  // текущая дата в милисекундах с 01.01.1970
-        long validDayTime = todayTime + (3*24*60*60*1000); // прибавляю к текущей дате 3 дня в милисекундах
-        String validDate = format.format(validDayTime);    // форматирование из милисекунд в дату
+
+        String planningDate = generateDate(5);
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").sendKeys("Санкт-Петербург");
-        $("[data-test-id='date'] input").sendKeys(validDate);
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
 
         $("[data-test-id='name'] input").sendKeys("Кузьма Петров-Водкин");
         $("[data-test-id='phone'] input").sendKeys("+79000000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(withText("Забронировать")).click();
 
-        $x("//*[contains(text(), 'Успешно!')]").should(Condition.appear, Duration.ofSeconds(20));
-        $x("//*[contains(text(), 'Встреча успешно забронирована на')]").should(Condition.appear, Duration.ofSeconds(20));
+        $x("//*[contains(text(), 'Успешно!')]").should(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
 
 
 
+    }
 
-
-
-
-
-
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
